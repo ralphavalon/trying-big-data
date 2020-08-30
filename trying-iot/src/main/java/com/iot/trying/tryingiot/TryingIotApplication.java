7a50363191671690803bd7b751350ec10d77fbb3
Raphael Amoedo <avalon.ralph@gmail.com>
2020-08-30 23:10:20 +0000

    Add iot producer

--- trying-iot/src/main/java/com/iot/trying/tryingiot/TryingIotApplication.java
+++ trying-iot/src/main/java/com/iot/trying/tryingiot/TryingIotApplication.java
@@ -1,15 +1,35 @@
 package com.iot.trying.tryingiot;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.UUID;
 
 @SpringBootApplication
 @EnableScheduling
 public class TryingIotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TryingIotApplication.class, args);
-	}
+	@Value("${mqtt.url}")
+	private String url;
+
+	private String clientId = UUID.randomUUID().toString();
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryingIotApplication.class, args);
+    }
+
+    @Bean
+    public MqttClient mqttClient() throws MqttException {
+        String broker = this.url;
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        return new MqttClient(broker, clientId, persistence);
+    }
 
 }
--- trying-iot/src/main/java/com/iot/trying/tryingiot/quartz/SenderJob.java
+++ trying-iot/src/main/java/com/iot/trying/tryingiot/quartz/SenderJob.java
@@ -1,6 +1,12 @@
 package com.iot.trying.tryingiot.quartz;
 
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +14,52 @@ import org.springframework.stereotype.Component;
 @Component
 public class SenderJob {
 
+    @Autowired
+    private MqttClient mqttClient;
+
+    @Value("${mqtt.topic}")
+    private String topic;
+
+    @Value("${mqtt.qos}")
+    private Integer qos;
+
     @Scheduled(fixedDelayString = "${delay-in-milliseconds}")
     public void execute() {
-        log.info("Running");
+        String content = String.valueOf(getRandomIntegerBetweenRange(30, 40));
+
+        try {
+            connect();
+            publishMessage(topic, content, qos);
+            disconnect();
+        } catch (MqttException me) {
+            log.error(me.getMessage(), me);
+        }
     }
+
+    private int getRandomIntegerBetweenRange(int min, int max){
+        return (int) ((Math.random()*((max-min)+1))+min);
+    }
+
+    private void connect() throws MqttException {
+        MqttConnectOptions connOpts = new MqttConnectOptions();
+        connOpts.setCleanSession(true);
+        log.info("Connecting to broker: " + mqttClient.getServerURI());
+        mqttClient.connect(connOpts);
+        log.info("Connected");
+    }
+
+    private void publishMessage(String topic, String content, int qos) throws MqttException {
+        log.info("Publishing message: " + content);
+        MqttMessage message = new MqttMessage(content.getBytes());
+        message.setQos(qos);
+        mqttClient.publish(topic, message);
+        log.info("Message published");
+    }
+
+    private void disconnect() throws MqttException {
+        log.info("Disconnecting");
+        mqttClient.disconnect();
+        log.info("Disconnected");
+    }
+
 }
